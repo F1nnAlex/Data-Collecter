6ee1916198f5d5db3d449df1a877b36fa29ab989
Alex Davies <alexodavies05@gmail.com>
2024-03-18 21:41:11 +0200

    Update Main.py
    Added button, text box, label, button fuction to show result

--- Data collection Project/Main.py
+++ Data collection Project/Main.py
@@ -1,19 +1,45 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
 
+class my_window(QMainWindow):
+    def __init__(self):
+        super(my_window, self).__init__()
+        # Place window closer to center and resize when program is run
+        self.setGeometry(500, 500, 500, 300)
+        # Change application name
+        self.setWindowTitle("Data Collector")
+        # If hovering over nothing add "Data" tooltip
+        self.setToolTip("Data")
+        self.initUI()
 
+    def initUI(self):
+        self.input_lbl = QtWidgets.QLabel(self)
+        self.input_lbl.setText("Give us data...")
+        self.input_lbl.move(100, 50)
+
+        self.input_box = QtWidgets.QLineEdit(self)
+        self.input_box.move(200, 50)
+        self.input_box.resize(200,32)
+
+        self.save_btn = QtWidgets.QPushButton(self)
+        self.save_btn.setText("Save")
+        self.save_btn.clicked.connect(self.clicked)
+        self.save_btn.move(150, 80)
+
+        self.result_lbl = QtWidgets.QLabel(self)
+        self.result_lbl.setText("Saved Data")
+        self.result_lbl.move(250, 50)
+        self.result_lbl.resize(200, 200)
+
+    def clicked(self):
+        self.result_lbl.setText(("Your Data"))
 # Create a fucntion for the window
 def window():
     # Collect all arguements to run code
     app = QApplication(sys.argv)
     # Create a visual window
-    win = QMainWindow()
-    # Place window closer to center and resize when program is run
-    win.setGeometry(500, 500, 500, 300)
-    # Change application name
-    win.setWindowTitle("Data Collector")
-    # If hovering over nothing add "Data" tooltip
-    win.setToolTip("Data")
+    win = my_window()
     # Makes window visible
     win.show()
     # Exits program if x is pressed
@@ -21,6 +47,4 @@ def window():
 
 
 # Run "window" function
-
-
 window()
