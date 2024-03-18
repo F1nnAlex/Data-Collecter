601502dc72164a05de38ad25a2be418328b994ee
Alex Davies <alexodavies05@gmail.com>
2024-03-18 21:00:55 +0200

    Cleaned up code

--- Data collection Project/Main.py
+++ Data collection Project/Main.py
@@ -1,24 +1,26 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-#Create a fucntion for the window
+
+# Create a fucntion for the window
 def window():
-    #Collect all arguements to run code
+    # Collect all arguements to run code
     app = QApplication(sys.argv)
-    #Create a visual window
+    # Create a visual window
     win = QMainWindow()
-    #Place window closer to center and resize when program is run
-    win.setGeometry(500,500,500,300)
-    #Change application name
+    # Place window closer to center and resize when program is run
+    win.setGeometry(500, 500, 500, 300)
+    # Change application name
     win.setWindowTitle("Data Collector")
-    #If hovering over nothing add "Data" tooltip
+    # If hovering over nothing add "Data" tooltip
     win.setToolTip("Data")
-    #Makes window visible
+    # Makes window visible
     win.show()
-    #Exits program if x is pressed
+    # Exits program if x is pressed
     sys.exit(app.exec_())
 
-#Run "window" function
+
+# Run "window" function
 
 
 window()
