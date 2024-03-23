fc3041fd3d91563fd79b15b61059ae740dfdff1a
Alex Davies <alexodavies05@gmail.com>
2024-03-23 18:49:31 +0200

    Update
    Changed layout to grid to help with excel data and buttons being nicely.

--- Data collection Project/Main.py
+++ Data collection Project/Main.py
@@ -1,50 +1,76 @@
+from PyQt5.QtWidgets import QApplication, QPushButton, QGridLayout, QLabel, QLineEdit,QHBoxLayout, QVBoxLayout, QTableWidget, QTableWidgetItem, QWidget
 import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import *
+import openpyxl
 
-class my_window(QMainWindow):
+
+class Main(QWidget):
     def __init__(self):
-        super(my_window, self).__init__()
+        super(Main, self).__init__()
         # Place window closer to center and resize when program is run
         self.setGeometry(500, 500, 500, 300)
         # Change application name
         self.setWindowTitle("Data Collector")
         # If hovering over nothing add "Data" tooltip
         self.setToolTip("Data")
-        self.initUI()
 
-    def initUI(self):
-        self.input_lbl = QtWidgets.QLabel(self)
-        self.input_lbl.setText("Give us data...")
-        self.input_lbl.move(100, 50)
+        input_lbl = QLabel(self)
+        input_lbl.setText("Give us data...")
 
-        self.input_box = QtWidgets.QLineEdit(self)
-        self.input_box.move(200, 50)
-        self.input_box.resize(200,32)
+        self.input_box = QLineEdit(self)
 
-        self.save_btn = QtWidgets.QPushButton(self)
-        self.save_btn.setText("Save")
-        self.save_btn.clicked.connect(self.clicked)
-        self.save_btn.move(150, 80)
+        save_btn = QPushButton()
+        save_btn.setText("Save")
+        save_btn.clicked.connect(self.on_click)
 
-        self.result_lbl = QtWidgets.QLabel(self)
-        self.result_lbl.setText("Saved Data")
-        self.result_lbl.move(250, 50)
-        self.result_lbl.resize(200, 200)
+        grid = QGridLayout()
 
-    def clicked(self):
-        self.result_lbl.setText(("Your Data"))
-# Create a fucntion for the window
-def window():
-    # Collect all arguements to run code
+        layout = QVBoxLayout()
+        Vlayout = QVBoxLayout()
+        self.setLayout(grid)
+
+        self.table_widget = QTableWidget()
+        layout.addWidget(self.table_widget)
+        Vlayout.addWidget(input_lbl)
+        Vlayout.addWidget(self.input_box)
+        Vlayout.addWidget(save_btn)
+
+        grid.addLayout(layout, 0, 0)
+        grid.addLayout(Vlayout, 0, 1)
+
+        self.load_data()
+
+    def on_click(self):
+        self.input_box.clear()
+        self._addRow()
+
+    def _addRow(self):
+        rowCount = self.table_widget.rowCount()
+        self.table_widget.insertRow(rowCount)
+        self.load_data()
+
+    def load_data(self):
+        path = "data.xlsx"
+        workbook = openpyxl.load_workbook(path)
+        sheet = workbook.active
+
+        # Set size of excel sheet
+        self.table_widget.setRowCount(sheet.max_row)
+        self.table_widget.setColumnCount(sheet.max_column)
+
+        list_values = list(sheet.values)
+        self.table_widget.setHorizontalHeaderLabels(list_values[0])
+
+        row_index = 0
+        for value_tuple in list_values[1:]:
+            col_index = 0
+            for value in value_tuple:
+                self.table_widget.setItem(row_index,col_index,QTableWidgetItem(str(value)))
+                col_index += 1
+            row_index += 1
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    # Create a visual window
-    win = my_window()
-    # Makes window visible
-    win.show()
-    # Exits program if x is pressed
-    sys.exit(app.exec_())
-
-
-# Run "window" function
-window()
+    window = Main()
+    window.show()
+    app.exec_()
